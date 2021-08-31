347a8ca281233defd9ff1fde71268ad025268d67
Kale <13501991+AlbusNoir@users.noreply.github.com>
2021-08-31 11:10:53 -0400

    initial

--- README.md
+++ README.md
@@ -1,2 +1,152 @@
-# IPDL
-IP &amp; Domain Lookup
+<!-- PROJECT SHIELDS -->
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![License][license-shield]][license-url]
+
+
+<!-- PROJECT LOGO -->
+<br />
+<h3 align="center">IPDL</h3>
+<p align="center">
+  <br />
+  IPDL: IP & Domain Lookup<br>
+  IPDL offers a quick way to scan IP addresses and domains to detect potetially malicious actors
+</p>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary><h2 style="display: inline-block">Table of Contents</h2></summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#languages-and-frameworks">Languages and Frameworks</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgements">Acknowledgements</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+![Demo](demo.gif)
+
+
+### Languages and Frameworks
+
+* [Python3.9](https://www.python.org/)*
+
+*Python3.6 or above is required due to the use of f-strings
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+### Prerequisites
+
+This is an example of how to list things you need to use the software and how to install them.
+* Install packages
+  ```
+  pip install pydnsbl
+  ```
+
+### Installation
+
+1. Clone the repo
+   ```
+   git clone https://github.com/github_username/repo.git
+   ```
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+IPDL has two modes: verbose and fast(non-verbose). Verbose will provide you with more information than fast will._
+
+
+
+<!-- ROADMAP -->
+## Roadmap
+_Current_
+- [x] Make the thing 
+- [ ] should there be color (green v red) if detected/not? 
+- [ ] Brightcloud and VirusTotal sometimes differ in detection, is this an issue?
+- [ ] Current limitations: adding / to a url breaks the code so it can't scan beyond site.com
+
+_Future Plans_
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+If you want to make contributions:
+
+1. Fork this Repository
+2. Create your Branch(`git checkout -b <branch name>`)
+3. Commit your Changes(`git commit -m 'I did some things'`)
+4. Push to the Branch(`git push origin <project_name>/<location>`)
+5. Open a Pull Request
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the GPLv3 License. See [`LICENSE INFO`](https://choosealicense.com/) for more information.
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Name | Social | Email |
+------------ | ------------- | ------------- |
+Name | [![Twitter][twitter-shield]][twitter-url] | <a href="mailto:kalegithub@gmail.com"><img src="https://img.shields.io/badge/-email-Email?style=for-the-badge&logo=gmail&colorB=555"></a>
+
+
+
+<!-- ACKNOWLEDGEMENTS -->
+## Acknowledgements & Contributions
+
+#### Thank you to the following:
+
+User | Contribution |
+------------ | ------------- |
+
+
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/albusnoir/IPDL.svg?style=for-the-badge
+[contributors-url]: https://github.com/albusnoir/IPDL/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/albusnoir/IPDL.svg?style=for-the-badge
+[forks-url]: https://github.com/albusnoir/IPDL/network/members
+[stars-shield]: https://img.shields.io/github/stars/albusnoir/IPDL.svg?style=for-the-badge
+[stars-url]: https://github.com/albusnoir/IPDL/stargazers
+[license-shield]: https://img.shields.io/github/license/albusnoir/IPDL.svg?style=for-the-badge
+[license-url]: https://github.com/albusnoir/IPDL/blob/master/LICENSE
+[twitter-shield]: https://img.shields.io/badge/-twitter-Twitter?style=for-the-badge&logo=twitter&colorB=555
+[twitter-url]: https://twitter.com/kaleleafygreen
---
+++ ipdl.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+"""
+IPD_LOOKUP TOOL
+Utilizes PyDNSBL to determine if an IP or domain is malicious or not
+"""
+
+import pydnsbl
+from pydnsbl.providers import BASE_PROVIDERS, Provider  # Provider is for user-made providers
+
+__author__ = "Kaleb Sego"
+__copyright__ = "Copyright 2021"
+__license__ = "GPLv3"
+__version__ = "1.0.1"
+__maintainer__ = "Kaleb Sego"
+__contact__ = "https://github.com/AlbusNoir"
+
+
+providers = BASE_PROVIDERS  # if additional providers, use below
+# providers = BASE_PROVIDERS + [Provider('theprovider.com'),...]
+ip_checker = pydnsbl.DNSBLIpChecker(providers = providers)
+domain_checker = pydnsbl.DNSBLDomainChecker()
+
+
+def main():
+    """Main driver function"""
+    print('''
+     _____ ______ _____   _       
+    (_____|_____ (____ \ | |      
+       _   _____) )   \ \| |      
+      | | |  ____/ |   | | |      
+     _| |_| |    | |__/ /| |_____ 
+    (_____)_|    |_____/ |_______)
+    -------------------------------
+    IP & Domain Lookup
+    1) Run verbose
+    2) Run non-verbose
+    3) Help
+    4) Quit
+    ''')
+
+    while True:
+        i = input('> ')
+
+        if i == '1':
+            verbose()
+            break
+        elif i == '2':
+            non_verbose()
+            break
+        elif i == '3':
+            print('''
+            IPDLookup is a tool that accepts IP addresses and domains and runs them through various databases to 
+            determine if they are malicious or not.
+            
+            "Malicious" is determined by the existence of an IP in a database. Just because an IP or domain is in a 
+            database does not mean it is inherently harmful. Further investigation is still needed.
+            
+            IPDLookup runs in two modes: verbose and non-verbose.
+            Verbose will output the following: blacklist(T/F), which blacklist the IP/domain is in, the category the 
+            IP/domain was assigned to, and a list of providers(databases) the IP/domain was scanned through
+            
+            Non-verbose will output the following: blacklist(T/F), which blacklist the IP/Domain is in
+            ''')
+        elif i == '4':
+            break
+        else:
+            print(f'Input Error: {i} not an option')
+
+
+def verbose():
+    """If verbose mode is selected"""
+    print('''
+    MODE: VERBOSE
+    1) IP Lookup
+    2) Domain Lookup
+    3) Back
+    4) Quit     
+   ''')
+
+    while True:
+        i = input('IPDL:M=V> ')
+
+        if i == '1':
+            verbose_lookup_ip()
+        elif i == '2':
+            verbose_lookup_domain()
+        elif i == '3':
+            main()
+            break
+        elif i == '4':
+            break
+        else:
+            print(f'Input Error: {i} not an option')
+
+
+def non_verbose():
+    """Non-verbose"""
+    print('''
+    MODE: FAST
+    1) IP Lookup
+    2) Domain Lookup
+    3) Back
+    4) Quit     
+   ''')
+
+    while True:
+        i = input('IPDL:M=F> ')
+
+        if i == '1':
+            non_verbose_lookup_ip()
+        elif i == '2':
+            non_verbose_lookup_domain()
+        elif i == '3':
+            main()
+            break
+        elif i == '4':
+            break
+        else:
+            print(f'Input Error: {i} not an option')
+
+
+def verbose_lookup_ip():
+    """Perform ip lookup for verbose mode"""
+    ip = input('Enter IP address(s), separated by a comma, no spaces: ')
+
+    ip_list = ip.split(",")
+
+    for ip in ip_list:
+
+        result = ip_checker.check(ip)
+        caught_by = result.detected_by if result.blacklisted else 'N/A'
+        bl = result.blacklisted
+        cat = result.categories if result.blacklisted else 'N/A'
+        prov = result.providers
+
+        print(f'''RESULTS of {ip}:
+        Blacklisted: {bl}
+        Detected By: {caught_by}
+        Categories: {cat}
+        Scanned By: {prov}
+        ''')
+
+
+def verbose_lookup_domain():
+    """Perform domain lookup for verbose mode"""
+    domain = input('Enter domain(s), separated by a comma, no spaces: ')
+
+    domain_list = domain.split(",")
+
+    for domain in domain_list:
+
+        result = domain_checker.check(domain)
+        caught_by = result.detected_by if result.blacklisted else 'N/A'
+        bl = result.blacklisted
+        cat = result.categories if result.blacklisted else 'N/A'
+        prov = result.providers
+
+        print(f'''RESULTS of {domain}:
+                Blacklisted: {bl}
+                Detected By: {caught_by}
+                Categories: {cat}
+                Scanned By: {prov}
+        ''')
+
+
+def non_verbose_lookup_ip():
+    """Perform ip lookup for non-verbose mode"""
+    ip = input('Enter IP address(s), separated by a comma, no spaces: ')
+
+    ip_list = ip.split(",")
+
+    for ip in ip_list:
+
+        result = ip_checker.check(ip)
+        bl = result.blacklisted
+        detected = result.detected_by
+
+        print(f'''RESULTS of {ip}:
+        Blacklisted: {bl}
+        Detected By: {detected}
+        
+        For more information, run verbose mode
+        ''')
+
+
+def non_verbose_lookup_domain():
+    """Perform domain lookup for non-verbose mode"""
+    domain = input('Enter domain(s), separated by a comma, no spaces: ')
+
+    domain_list = domain.split(",")
+
+    for domain in domain_list:
+
+        result = domain_checker.check(domain)
+        bl = result.blacklisted
+        detected = result.detected_by
+
+        print(f'''RESULTS of {domain}:
+        Blacklisted: {bl}
+        Detected By: {detected}
+        
+        For more information, run verbose mode
+        ''')
+
+
+if __name__ == '__main__':
+    main()
